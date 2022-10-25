a904d36e4029c7c5123638cf59075d100beec1a0
Abdullah <adaoub0@gmail.com>
2022-10-25 10:49:45 +0100

    Statement array is updated every time a transcation is made

--- bankAccount.js
+++ bankAccount.js
@@ -8,13 +8,28 @@ class BankAccount {
     this.balance += amount;
     this.date = date;
     this.credit = amount;
+
+    this.statement.push({
+      date: this.date,
+      credit: this.credit,
+      debit: "",
+      balance: this.balance,
+    });
   };
 
   makeWithdraw = (amount, date) => {
     this.balance -= amount;
     this.date = date;
     this.debit = amount;
+
+    this.statement.push({
+      date: this.date,
+      credit: "",
+      debit: this.debit,
+      balance: this.balance,
+    });
   };
+
   getBalance = () => {
     return this.balance;
   };
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -3,14 +3,32 @@ const Bank = require("./bankAccount");
 describe("BankAccount", () => {
   it("user can make a deposit and the total balance is incremented", () => {
     const bank = new Bank();
-    bank.makeDeposit(1000);
+    bank.makeDeposit(1000, "10/01/2012");
     expect(bank.getBalance()).toEqual(1000);
+  });
+
+  it("check if deposit transaction is added to statement array", () => {
+    const bank = new Bank();
+    bank.makeDeposit(1000, "10/01/2012");
+    expect(bank.statement).toEqual([
+      { date: "10/01/2012", credit: 1000, debit: "", balance: 1000 },
+    ]);
+  });
+
+  it("check if withdraw transaction is added to statement array", () => {
+    const bank = new Bank();
+    bank.makeDeposit(1000, "10/01/2012");
+    bank.makeWithdraw(500, "13/01/2012");
+    expect(bank.statement).toEqual([
+      { date: "10/01/2012", credit: 1000, debit: "", balance: 1000 },
+      { date: "13/01/2012", credit: "", debit: 500, balance: 500 },
+    ]);
   });
 
   it("user can make a withdraw and the total balance in decremneted", () => {
     const bank = new Bank();
-    bank.makeDeposit(1000);
-    bank.makeWithdraw(500);
+    bank.makeDeposit(1000, "10/01/2012");
+    bank.makeWithdraw(500, "13/01/2012");
     expect(bank.getBalance()).toEqual(500);
   });
 });
